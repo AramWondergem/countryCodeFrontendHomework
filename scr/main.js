d52eaf4e5abfca2295ac2c03cf802250958bc8df
AramWondergem <aramwondergem@gmail.com>
2022-11-09 16:27:42 +0100

    finished part 1

--- scr/main.js
+++ scr/main.js
@@ -1,25 +1,56 @@
-// voorbeeld voor gebruiken package
+// todo importing the data from world api
+import axios from "axios";
 
-import giveMeAJoke from 'give-me-a-joke';
+(async function fetchData () {
+    try {
+        const countriesData = await axios.get('https://restcountries.com/v3.1/all');
+        console.log(countriesData)
+        countriesData.data.sort((a,b) =>  a.population - b.population);
 
-giveMeAJoke.getRandomDadJoke((joke)=> {
-    console.log(joke);
+        countriesData.data.map((entryData) => {
 
-    //voorbeeld voor testen
-})
-function add(number1,number2) {
-    return number1+number2;
-}
+            const{ name: { common }, flags:{ png }, population, continents} = entryData;
+            const[mainContinent, ...rest] = continents;
 
-function findByName(array, user){
-    const result = array.find((name) => name.name === user);
-    if (result) {
-        return result
-    } else {
-        return null;
+            const countrySection = document.getElementById('container-country-information')
+
+            const country = document.createElement("article");
+            country.setAttribute("class", "country");
+            countrySection.appendChild(country);
+
+            const flagAndTitleWrapper = document.createElement('div');
+            flagAndTitleWrapper.setAttribute('class','flag-and-title-wrapper');
+
+            const p = document.createElement('p');
+
+            p.textContent = `Has a population of ${population} people`;
+
+            country.appendChild(flagAndTitleWrapper);
+            country.appendChild(p)
+
+            const imageWrapper = document.createElement('div');
+            imageWrapper.setAttribute('class','imagewrapper');
+
+            const h2 = document.createElement("h2");
+            h2.setAttribute('class',mainContinent);
+
+            h2.textContent = common;
+
+            flagAndTitleWrapper.appendChild(imageWrapper);
+            flagAndTitleWrapper.appendChild(h2)
+
+            const flag = document.createElement("img");
+            flag.setAttribute('src', png);
+            flag.setAttribute('alt', 'flag');
+
+            imageWrapper.appendChild(flag);
+
+
+
+        })
+
+
+    } catch (e){
+
     }
-}
-module.exports = {
-    add: add,
-    findByName,
-}
+})()
